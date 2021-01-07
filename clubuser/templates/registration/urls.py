13ebff7fea91432a3088d1c287dfe34a4547f186
ruhollahmozafari <roohollah.m.1998@gmail.com>
2021-01-07 23:32:58 +0330

    adding the forgot-password, fixing bugs

---
+++ clubuser/templates/registration/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from .views import *
+from django.views.generic import TemplateView
+
+# from blog.views import views as question_views
+app_name='clubuser'  
+
+urlpatterns = [
+    path('profile/<int:pk>/',profile, name = 'profile'),
+    path('signup/',UserCreation.as_view(), name= 'signup'),
+    path('login/', login, name= 'login'),
+    path('logout/',logout , name = 'logout'),
+    path('update-profile/<int:pk>/', edit_profile, name = 'update-profile'),
+    # path('profile/change-pass/', PasswordChangeView.as_view(template_name = 'registration/change-pass.html'), name= 'change-pass')
+    path('profile/change-pass/', PassChangeView.as_view(), name= 'change-pass'),
+    path('profile/pass-changed/',TemplateView.as_view(template_name = 'clubuser/pass-changed.html'), name = 'pass-changed' ),
+    # path('clubuser/', include('django.contrib.auth.urls')),
+]
+
+
+
--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from clubuser import views as clubuser_views
+
 
 
 urlpatterns = [
@@ -10,5 +13,11 @@ urlpatterns = [
     path('',include('blog.urls', namespace='blog')),
     path('',include('clubuser.urls',namespace='clubuser')),
     path('hitcount/', include(('hitcount.urls', 'hitcount'), namespace='hitcount')),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='layouts/password/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="layouts/password/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='layouts/password/password_reset_complete.html'), name='password_reset_complete'),      
+    path("password_reset/", clubuser_views.password_reset_request, name="password_reset"),
     
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
